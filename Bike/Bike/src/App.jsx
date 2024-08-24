80893d777d772b6ddd17b898f99fec17cd4ff44a
Vutha Soy <V010utha@gamil.com>
2024-08-24 16:15:13 +0700

    diverse path

--- Bike/Bike/src/App.jsx
+++ Bike/Bike/src/App.jsx
@@ -4,10 +4,12 @@ import {Subscribe} from '../src/SECTIONS/Subscribe';
 import {Arival} from '../src/SECTIONS/Arival';
 import {Hero} from '../src/SECTIONS/Hero';
 import {Nav} from '../src/SECTIONS/Nav';
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {Detail} from './SECTIONS/Detail';
 
 export default function App() {
   return (
+      <BrowserRouter>
     <main className="relative">
       <nav><Nav/></nav>
       <section className="xl:padding-1 wide:padding-r border paading-b px-5 max-sm:px-1"><Hero/></section>
@@ -15,6 +17,11 @@ export default function App() {
       <section className="px-5"><Explore/></section>
       <section className="py-10"><Subscribe/></section>
       <section className="padding bg-black"><Footer/></section>
+       <Routes>
+        <Route  element={<Detail />} />
+      </Routes>
     </main>
+    
+    </BrowserRouter>
   )
 }
