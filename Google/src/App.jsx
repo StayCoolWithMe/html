6a2dd5919d117f9c562b69e6ec331a30947e32c7
Vutha Soy <V010utha@gmail.com>
2024-10-09 20:29:08 +0700

    initialize google

---
+++ Google/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import Navbar from './Component/Navbar'
+import Footer from './Component/Footer'
+import Routes from './Component/Routes'
+
+const App = () => {
+  const [darktheme,setDarktheme]=useState(false);
+  return (
+    <div  className={darktheme?'dark':''}>
+      <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+
+      <Navbar darktheme={darktheme} setDarktheme={setDarktheme}/>
+      <Footer/>
+      <Routes/>
+      </div>
+    </div>
+  )
+}
+
+export default App
